5ec058c0b74dc05ed9e3a7657c019bf78614d865
lanjarset1awan <lanjarsetiawan3@gmail.com>
2026-05-10 10:42:15 +0700

    fix softdelete

--- backend/src/routes/animalRoutes.js
+++ backend/src/routes/animalRoutes.js
@@ -4,11 +4,11 @@ import animalController from '../controllers/animalController.js';
 const router = express.Router();
 
 router.get('/', animalController.index);
+router.get('/deleted', animalController.getDeleted);
 router.get('/:id', animalController.show);
 router.post('/', animalController.store);
 router.put('/:id', animalController.update);
 router.delete('/:id', animalController.destroy);
-router.get('/deleted', animalController.getDeleted);
 router.delete('/:id/force', animalController.forceDelete);
 router.put('/:id/restore', animalController.restore);
 
